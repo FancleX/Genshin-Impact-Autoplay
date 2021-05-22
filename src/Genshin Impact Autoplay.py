fc8caf5e9806fb586b343fbad4663c5c6d663b53
Zhicun Chen <fancle666@gmail.com>
2021-05-22 18:40:42 -0400

    Version 2

--- src/Genshin Impact Autoplay.py
+++ src/Genshin Impact Autoplay.py
@@ -1,47 +1,75 @@
 import pyautogui
 import re
 import time
+import os
 
+#First filter lyrics with combo keys, single keys and space pause
 def lyricsfinder(lyrics):
     ls = []
     for rows in lyrics:
-        rows = rows.replace('\n', '')
+        rows = rows.replace('\n', ' ')
         ls.append(re.findall(r'[\w\s]+', rows))
     return ls
 
+#Autoplay via simulating pressing keys on keyboard
+def autoplay(keys, t_pause, t_single, t_combo):
+    #Delay of pause
+    if (keys == ' '):
+        return time.sleep(t_pause)
+                
+    #Delay of single key press
+    elif (len(keys) == 1):
+        return pyautogui.press(keys), time.sleep(t_single)
+        
+    #Delay of combo keys press
+    elif (len(keys) > 1 and not ' ' in keys):
+        s = []
+        for letter in keys:
+            s.append(letter)
+        return pyautogui.press(s), time.sleep(t_combo)
+                
+    #Delay of single key and pause
+    else:
+        ele = []
+        ele = list(keys)
+        for i in ele:
+            if (i == ' '):
+                return time.sleep(t_pause)
+            else:
+                return pyautogui.press(i), time.sleep(t_single)
+
 def main():
+    check = False
+    while (check == False):
+        try:
+            with open(input('Music name: '), 'r') as f:
+                lyrics = f.readlines()
+            check = True
+        except:
+            print('Name input error, please retry')
+            pass
+
     try:
-        with open(input('Music name: '), 'r') as f:
-            lyrics = f.readlines()
-            time.sleep(3)
+        print('Press 1 to set delay time, 2 to keep defalt')
+        if (eval(input()) == 1):
+            t_pause = eval(input('Please set pause delay time(s): '))
+            t_single = eval(input('Please set single key delay time(s): '))
+            t_combo = eval(input('Please set combo keys delay time(s): '))
+        else:
+            t_pause = 0.1
+            t_single = 0.07
+            t_combo = 0.08
     except:
-        print('Name input error')
+        print('Input error')
 
     try: 
-        print('----Start----')
+        print('{0:=^20}'.format('Start'))
+        time.sleep(3)
         for row in lyricsfinder(lyrics):
             for keys in row:
-                if (keys == ' '):
-                    time.sleep(0.01)
-                elif (len(keys) == 1):
-                    pyautogui.press(keys)
-                    time.sleep(0.08)
-                elif (len(keys) > 1 and not ' ' in keys):
-                    s = []
-                    for letter in keys:
-                        s.append(letter)
-                    pyautogui.press(s)
-                    time.sleep(0.1)
-                else:
-                    ele = []
-                    ele = list(keys)
-                    for i in ele:
-                        if (i == ' '):
-                            time.sleep(0.005)
-                        else:
-                            pyautogui.press(i)
-                            time.sleep(0.08)
-        print('----End----')
+                autoplay(keys, t_pause, t_single, t_combo)
+        print('{0:=^20}'.format('End'))
+        os.system('pause')
     except:
         print('Format of lyrics is wrong')
 
